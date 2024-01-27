c0091dd159ecb2b4edd33d6f55126d933fcb5cfa
Gautam-10137 <pahwagautam786@gmail.com>
2024-01-27 21:43:11 +0530

    middleware added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
+const cors=require('cors');
 require('dotenv').config();
 const PORT=process.env.PORT;
 const app=express();
 
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(cors({
+    origin:'http://localhost:3000',
+    method:['GET','POST']
+}));
 
 app.listen(PORT,()=>{
     console.log('Server is listening on port: '+PORT);
